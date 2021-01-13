581af13bd69191bd40878eb4636e1accde1e6b62
Bes <besartshyti@gmail.com>
2021-01-13 14:07:59 +0100

    [init]

--- README.md
+++ README.md
@@ -1,70 +1,70 @@
-# Getting Started with Create React App
+## Pinterest Layout
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## Available Scripts
+```js
+/**
+ *
+ *
+ *
+ */
+const rawData = [
+  { time: 250, height: 400, id: '01' },
+  { time: 240, height: 200, id: '02' },
+  { time: 230, height: 450, id: '03' },
+  { time: 220, height: 400, id: '04' },
+  { time: 210, height: 100, id: '05' },
+  { time: 200, height: 300, id: '06' },
+  { time: 190, height: 400, id: '07' },
+  { time: 180, height: 600, id: '08' },
+  { time: 170, height: 300, id: '09' },
+  { time: 160, height: 200, id: '10' },
+  { time: 150, height: 600, id: '11' },
+];
 
-In the project directory, you can run:
 
-### `yarn start`
+/**
+ *
+ *
+ *
+ */
+const rowData = [
+  [ {}, {}, {} ],
+  [ {}, {}, {} ],
+  [ {}, {}, {} ],
+  [ {}, {}, {} ],
+  // ...
+];
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### `yarn test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `yarn build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `yarn build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+/**
+ *
+ *
+ *
+ */
+const data = [
+  [
+    {},
+    {},
+    {},
+    {},
+    {},
+    // ...
+  ],
+  [
+    {},
+    {},
+    {},
+    {},
+    {},
+    // ...
+  ],
+  [
+    {},
+    {},
+    {},
+    {},
+    {},
+    // ...
+  ]
+];
+```
--- src/App.js
+++ src/App.js
@@ -1,25 +1,234 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
 
-function App() {
+
+/**
+ *
+ *
+ *
+ */
+const DATA = [
+  { time: 250, height: 400, id: '01' },
+  { time: 240, height: 200, id: '02' },
+  { time: 230, height: 450, id: '03' },
+  { time: 220, height: 400, id: '04' },
+  { time: 210, height: 100, id: '05' },
+  { time: 200, height: 300, id: '06' },
+  { time: 190, height: 400, id: '07' },
+  { time: 180, height: 600, id: '08' },
+  { time: 170, height: 300, id: '09' },
+  { time: 160, height: 200, id: '10' },
+  { time: 150, height: 600, id: '11' },
+  { time: 150, height: 300, id: '12' },
+];
+
+
+/**
+ *
+ *
+ */
+export default function App() {
+
+  const rows = React.useMemo(() => groupByRow(DATA), []);
+  const cols = React.useMemo(() => groupByIdx(rows), []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <S.Container>
+      {cols.map(getColumnElm)}
+    </S.Container>
   );
+
+  /**
+   *
+   *
+   */
+  function getColumnElm(rows) {
+
+    return (
+      <S.Column>
+        {rows.map(getImageElm)}
+      </S.Column>
+    );
+  }
+
+  /**
+   *
+   *
+   *
+   */
+  function getImageElm(elm) {
+
+    const subProps = {
+      key: elm.id,
+      style: {height: elm.height},
+    };
+
+    return (
+      <S.InnerImage {...subProps}>
+        {elm.id}
+      </S.InnerImage>
+    );
+  }
+
+  /**
+   *
+   *
+   *
+   */
+  function groupByRow(innerData) {
+
+    return innerData.reduce((acc, elm, idx) => {
+
+      /**
+       *  initialize: first 3 elements will
+       *  always be positioned at the top
+       *
+       */
+      if (idx <= 2) {
+        acc[0] = acc[0] ?? [];
+        elm.acc = elm.height;
+        acc[0].push(elm);
+        return acc;
+      }
+
+      /**
+       *  idx modulo 3 equal 0, means
+       *  a new row is starting:
+
+          [
+            [400, 200, 500], <-- prevRow
+            []               <-- we are here
+          ]
+
+       *  find the min value index in prevRow
+       *  and place it in the same idx in currRow
+
+          [
+            [400, 200, 500], <-- it's 200 in idx 1
+            [   , XXX,    ], <-- so in idx 1 put XXX
+          ]
+       */
+      if (idx % 3 === 0) {
+
+        const prevRow = acc[acc.length - 1];
+        const currRow = [];
+
+        const accHeights = prevRow.map(elm => elm.acc);
+        const minAcc = Math.min(...accHeights);
+        const currMinIdx = accHeights.indexOf(minAcc);
+
+        elm.acc = elm.height + minAcc;
+        currRow[currMinIdx] = elm;
+        acc.push(currRow);
+        return acc;
+      }
+
+      /**
+       *  idx modulo 3 equal 1, means the
+       *  same as comment above. The real
+       *  only difference is find middle
+       *  value idx and insert it there
+
+          [
+            [400, 200, 500], <-- it's 400 in idx 0
+            [YYY, XXX,    ], <-- so in idx 1 put YYY
+          ]
+       */
+      if (idx % 3 === 1) {
+
+        const prevRow = acc[acc.length - 2];
+        const currRow = acc[acc.length - 1];
+
+        const accHeights = prevRow.map(elm => elm.acc);
+        const midAcc = [...accHeights].sort((a, b) => a - b)[1];
+        const currMidIdx = accHeights.indexOf(midAcc);
+
+        elm.acc = elm.height + midAcc;
+        currRow[currMidIdx] = elm;
+        return acc;
+      }
+
+      /**
+       *  finally, idx modulo 3 equal 2,
+       *  means find max value idx and
+       *  and insert curr element there
+
+          [
+            [400, 200, 500], <-- it's 500 in idx 0
+            [YYY, XXX, YYY], <-- so in idx 2 put ZZZ
+          ]
+       */
+      if (idx % 3 === 2) {
+
+        const prevRow = acc[acc.length - 2];
+        const currRow = acc[acc.length - 1];
+
+        const accHeights = prevRow.map(elm => elm.acc);
+        const maxAcc = Math.max(...accHeights);
+        const currMaxIdx = accHeights.indexOf(maxAcc);
+
+        elm.acc = elm.height + maxAcc;
+        currRow[currMaxIdx] = elm;
+        return acc;
+      }
+
+      return acc;
+    }, []);
+  }
+
+  /**
+   *  zip like function to transfrom,
+   *  it's basically a matrix rotation.
+   *  X => Y
+
+      X = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+      ];
+
+      Y = [
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+      ];
+   */
+  function groupByIdx(innerRows) {
+
+    const innerCols = [[], [], []];
+
+    innerRows.forEach(row => {
+      row.forEach((elm, idx) => {
+        innerCols[idx].push(elm);
+      });
+    });
+
+    return innerCols;
+  }
 }
 
-export default App;
+
+/**
+ *
+ *
+ */
+const S = {};
+
+S.Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 600px;
+`;
+
+S.Column = styled.div`
+  flex: 1;
+`;
+
+S.InnerImage = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 10px;
+  border: 1px solid;
+  width: 200px;
+`;
